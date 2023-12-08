0910d177eb6278ea7502871ff87b630beabbaaed
lijesh@yahoo.com <lijesh@yahoo.com>
2023-12-08 18:49:34 +0100

    eip added to ec2

--- main.tf
+++ main.tf
@@ -65,6 +65,17 @@ resource "aws_route53_record" "terraform" {
   name    = "${var.hostname}.${var.hosted_zone_name}"
   type    = "A"
   ttl     = 300
-  records = [aws_instance.frontend.public_ip]
+  records = [aws_eip.frontend.public_ip]
+}
+
+#------------------------------------------
+#Elastic IP for instance >> in new branch
+#------------------------------------------
+resource "aws_eip" "frontend" {
+  instance = aws_instance.frontend.id
+  domain   = "vpc"
+  tags = {
+    Name = "${var.project_name}-${var.project_env}-frontend"
+  }
 }
 
